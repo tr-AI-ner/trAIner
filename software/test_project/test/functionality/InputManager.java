b0f3b9d6d24a3e8e2d1ecc5c6aef74c314e43f6d
Patrick <yourpowrelectrifyfaq@gmail.com>
2018-11-22 15:12:51 +0100

    added basic test functionality, nothing implemented though...

---
+++ software/test_project/test/functionality/InputManager.java
@@ -0,0 +1,39 @@
+/** 
+  * Copyright 2018-11-22 Patrick
+  *
+  * All rights reserved
+  *
+  * @Author: Patrick
+  * 
+  * 
+  * Short description for what this file does...
+  * 
+  */
+package functionality;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class InputManager {
+
+
+    @Test
+    public void testInteger(){
+        int result = 5 * 10;
+        
+        assertEquals(50, result);
+    }
+
+    @Test
+    public void testString(){
+        String result = "the string";
+        assertEquals("the string", result);
+    }
+
+
+}
+
+
+
+
+
