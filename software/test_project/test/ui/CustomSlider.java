7771400b12bccf4adf8cf550021fdc866796afc4
Patrick <yourpowrelectrifyfaq@gmail.com>
2018-11-22 08:25:02 +0100

    added '._*' to gitignore (these unnecessary files get created by MacOS...), also added classes for JUnit tesetests

---
+++ software/test_project/test/ui/CustomSlider.java
@@ -0,0 +1,23 @@
+/** 
+  * Copyright 2018-11-22 Patrick
+  *
+  * All rights reserved
+  *
+  * @Author: Patrick
+  * 
+  * 
+  * Short description for what this file does...
+  * 
+  */
+package ui;
+
+public class CustomSlider {
+
+
+
+}
+
+
+
+
+
