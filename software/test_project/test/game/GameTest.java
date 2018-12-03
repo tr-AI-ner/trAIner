da4cae39a0f32b6d15bac31261c3ab228ff7e723
Patrick <yourpowrelectrifyfaq@gmail.com>
2018-12-03 22:59:48 +0100

    Experiments with unit testing

--- software/test_project/test/game/GameTest.java
+++ software/test_project/test/game/GameTest.java
@@ -11,10 +11,38 @@
   */
 package game;
 
-import org.junit.Test;
+import org.junit.*;
+
+import game.Game;
+import functionality.GraphicsManager;
+import functionality.InputManager;
+import map_builder.Map;
+
 import static org.junit.Assert.assertEquals;
 
+
+
 public class GameTest {
+
+    //Game game;
+    //GraphicsManager gm;
+    //InputManager inputManager;
+    //private Map map;
+
+    boolean theFuck;
+
+    @Before
+    public void setup(){
+        System.out.println("Setting up GameTest...");
+
+        Map map;// = new Map();
+        //map = new Map();
+        //this.inputManager = new InputManager();
+        //this.map = new Map();
+        //this.gm = new GraphicsManager(inputManager, map);
+        //this.game = new Game(gm);
+    }
+
 
 
     @Test
